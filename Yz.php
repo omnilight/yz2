25f7853d1ef539764776b5a00d7de3057da9e765
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-01-03 21:43:03 +0400

    Removed Messages component

---
+++ Yz.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace yz;
+
+
+/**
+ * Class Yz contains some helper static methods, constants and so on
+ * @package \yz
+ */
+class Yz 
+{
+	const FLASH_INFO = '__flash-info';
+	const FLASH_SUCCESS = '__flash-success';
+	const FLASH_WARNING = '__flash-warning';
+	const FLASH_ERROR = '__flash-error';
+}
