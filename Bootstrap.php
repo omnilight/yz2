0b1b551408642741bbc95978d207e03b7d775627
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-04-10 12:28:45 +0400

    Fixes

--- Bootstrap.php
+++ Bootstrap.php
@@ -15,7 +15,7 @@ class Bootstrap implements BootstrapInterface
      * Bootstrap method to be called during application bootstrap stage.
      * @param Application $app the application currently running
      */
-    public function bootstrap(Application $app)
+    public function bootstrap($app)
     {
         $app->i18n->translations['yz'] = [
             'class' => 'yii\i18n\PhpMessageSource',
